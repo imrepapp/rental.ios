a04d27d64993f248fc0bec1ef158760651e9d557
Krisztián KORPA <Krisztian.KORPA@xapt.com>
2019-02-28 14:37:00 +0100

    5403 - EMR Line binding and add missed outlet properties

--- Rental/ViewModels/EMRItemViewModel.swift
+++ Rental/ViewModels/EMRItemViewModel.swift
@@ -9,25 +9,45 @@ import RxCocoa
 
 class EMRItemViewModel: SimpleViewModel {
     let id = BehaviorRelay<String?>(value: nil)
-    let eqId = BehaviorRelay<String?>(value: nil)
     let emrId = BehaviorRelay<String?>(value: nil)
+
+    let eqId = BehaviorRelay<String?>(value: nil)
+    let quantity = BehaviorRelay<String?>(value: nil)
+
     let type = BehaviorRelay<String?>(value: nil)
-    let emrType = BehaviorRelay<Int>(value: 0)
     let direction = BehaviorRelay<String?>(value: nil)
-    let model = BehaviorRelay<String?>(value: nil)
-    let schedule = BehaviorRelay<String?>(value: nil)
-    let addressLabel = BehaviorRelay<String?>(value: nil)
-    let address = BehaviorRelay<String?>(value: nil)
-    let isScanned = BehaviorRelay<Bool>(value: false)
-    let isShipped = BehaviorRelay<Bool>(value: false)
-    let isReceived = BehaviorRelay<Bool>(value: false)
     let status = BehaviorRelay<String?>(value: nil)
+    let model = BehaviorRelay<String?>(value: nil)
     let serial = BehaviorRelay<String?>(value: nil)
+
+    let agreementType = BehaviorRelay<String?>(value: nil)
+    let agreement = BehaviorRelay<String?>(value: nil)
+
+    let from = BehaviorRelay<String?>(value: nil)
+    let fromAddress = BehaviorRelay<String?>(value: nil)
+
+    let to = BehaviorRelay<String?>(value: nil)
+    let toAddress = BehaviorRelay<String?>(value: nil)
+
+    let contact = BehaviorRelay<String?>(value: nil)
+    let phone = BehaviorRelay<String?>(value: nil)
+
+    let deliveryNotes = BehaviorRelay<String?>(value: nil)
+    let notes = BehaviorRelay<String?>(value: nil)
+
     let fuel = BehaviorRelay<String?>(value: nil)
     let smu = BehaviorRelay<String?>(value: nil)
     let secSMU = BehaviorRelay<String?>(value: nil)
-    let quantity = BehaviorRelay<String?>(value: nil)
+
+    let isScanned = BehaviorRelay<Bool>(value: false)
+    let isShipped = BehaviorRelay<Bool>(value: false)
+    let isReceived = BehaviorRelay<Bool>(value: false)
     let isNotReplaceableAttachment = BehaviorRelay<Bool>(value: true)
+
+    let emrType = BehaviorRelay<Int>(value: 0)
+    let schedule = BehaviorRelay<String?>(value: nil)
+    let addressLabel = BehaviorRelay<String?>(value: nil)
+    let address = BehaviorRelay<String?>(value: nil)
 
     convenience init() {
         self.init(RenEMRLine())
@@ -35,24 +55,53 @@ class EMRItemViewModel: SimpleViewModel {
 
     init(_ model: RenEMRLine) {
         self.id.val = model.id
-        self.eqId.val = model.equipmentId
+
         self.emrId.val = model.emrId
-        self.type.val = model.lineType
+
+        self.eqId.val = model.equipmentId
+        self.quantity.val = String(format:"%.1f", model.quantity)
+
         self.emrType.val = model.emrType
-        self.direction.val = "Inbound"
-        self.schedule.val = "Schedule"
-        self.addressLabel.val = model.addressLabel
-        self.address.val = model.address
-        self.direction.val = model.machineTypeId
-        self.isScanned.val = model.isScanned
-        self.isShipped.val = model.isShipped
-        self.isReceived.val = model.isReceived
-        self.status.val = model.emrStatus
+        self.direction.val = model.emr?.direction
+
+        self.model.val = model.machineTypeId
         self.serial.val = model.inventSerialId
+        self.status.val = model.emrStatus
+        self.agreementType.val = model.emr?.agreementRelationType
+        self.agreement.val = model.emr?.agreementRelation
+
+        self.from.val = model.emr?.fromRelationName
+        self.fromAddress.val = model.emr?.fromAddressDisplay
+
+        self.to.val = model.emr?.toRelationName
+        self.toAddress.val = model.emr?.toAddressDisplay
+
+        self.contact.val = model.emr?.customerContactName
+        self.phone.val = model.emr?.customerContactPhone
+
+        self.deliveryNotes.val = model.emr?.deliveryNote
+        self.notes.val = model.note
+
         self.fuel.val = String(format:"%.1f", model.fuelLevel)
         self.smu.val = String(format:"%.1f", model.smu)
         self.secSMU.val = String(format:"%.1f", model.secondarySMU)
-        self.quantity.val = String(format:"%.1f", model.quantity)
+
+        self.isScanned.val = model.isScanned
+        self.isShipped.val = model.isShipped
+        self.isReceived.val = model.isReceived
+
+        self.type.val = model.emr?.emrType
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        var deliveryDateString = ""
+        if let deliveryDate = model.emr?.deliveryDate {
+            deliveryDateString = formatter.string(from: deliveryDate)
+        }
+        self.schedule.val = deliveryDateString
+
+        self.addressLabel.val = model.addressLabel
+        self.address.val = model.address
     }
 
     func asModel() -> EMRLineModel {
